a8358ab18d379a87f6a7dcc9098d3e5e5c72cdd0
DaryaSmitt <Debora-ne@mail.ru>
2023-02-05 21:53:53 +0300

    !!

--- src/com/homework/task5/Matrix.java
+++ src/com/homework/task5/Matrix.java
@@ -1,19 +1,19 @@
 package com.homework.task5;
-import java.util.Random;
+
 public class Matrix {
-    private static final String[][] MATRIX = new String[10][10];
-    private static int i;
-    private static int j;
-    private static int num = 1;
+    public static final String[][] MATRIX = new String[10][10];
+    public static int i;
+    public static int j;
+    public static int number = 1;
     public static void createMatrix() {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                if (num % 3 == 0) {
+                if (number % 3 == 0) {
                     MATRIX[i][j] = RandomNN.randomNumber();
                 } else {
                     MATRIX[i][j] = RandomNN.randomString();
                 }
-                ++num;
+                ++number;
             }
         }
         System.out.println(MATRIX);
